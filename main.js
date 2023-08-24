578ca6da2d511e45707a9cb3d2d3b7facac520ff
Nick Moura <120116187+nickmoura@users.noreply.github.com>
2023-08-24 17:00:46 -0300

    responsiveness improved and only numbers at CPF

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const input = document.getElementById('cpf')
 
 input.addEventListener('keypress', () => {
 	let inputlength = input.value.length
+	const keyCode = event.which
+
+	if (keyCode < 48 || keyCode > 57) {
+		event.preventDefault()
+	  }
 
 	if (inputlength === 3 || inputlength === 7) {
 		input.value += '.'
