6015ca6b489fe6be18457ef2d6599380bcaa22ef
Nick Moura <120116187+nickmoura@users.noreply.github.com>
2023-08-25 01:10:03 -0300

    masks for cpf and mobile improved + pages added

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-const input = document.getElementById('cpf')
+const inputCpf = document.getElementById('cpf')
+if (inputCpf) {
+	inputCpf.addEventListener('keypress', () => {
+		let inputlength = inputCpf.value.length
+		const keyCode = event.which
 
-input.addEventListener('keypress', () => {
-	let inputlength = input.value.length
-	const keyCode = event.which
+		if (keyCode < 48 || keyCode > 57) {
+			event.preventDefault()
+		}
 
-	if (keyCode < 48 || keyCode > 57) {
-		event.preventDefault()
-	  }
+		if (inputlength === 3 || inputlength === 7) {
+			inputCpf.value += '.'
+		} else if (inputlength === 11) {
+			inputCpf.value += '-'
+		}
+	});
+}
 
-	if (inputlength === 3 || inputlength === 7) {
-		input.value += '.'
-	} else if (inputlength === 11) {
-		input.value += '-'
+
+const inputCel = document.getElementById('cel')
+if (inputCel) {
+
+	inputCel.addEventListener('input', () => {
+		const inputValue = inputCel.value.replace(/\D/g, ''); // Remove caracteres não-numéricos
+		const formattedValue = formatPhoneNumber(inputValue);
+		inputCel.value = formattedValue;
+	});
+
+	function formatPhoneNumber(value) {
+		const match = value.match(/^(\d{2})(\d{5})(\d{0,4})$/);
+		if (match) {
+			return `(${match[1]}) ${match[2]}${match[3] ? `-${match[3]}` : ''}`;
+		}
+		return value;
 	}
-})
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,17 @@
 # Maxima Multas
 
 Projeto que eu criei para meus estudos em HTML e CSS, onde desenvolvo páginas de login e outras páginas responsivas.
+
+## Funcionalidades da página Maxima Multas
+
+- 100% responsivo
+- Página de login
+- Página de cadastro
+    - Página de login e de cadastro com máscara para formatar CPF automaticamente e senha oculta
+- Página de "esqueci minha senha"
+    - Página de "esqueci minha senha" com máscara para formatar CPF automaticamente
+
+### Especificidades
+
+Senha oculta
+Máscara
